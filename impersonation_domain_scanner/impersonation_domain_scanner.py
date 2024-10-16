ef44ad317f88f888a629f6a40b1b909ea99d2d5d
Bobby Gray <34585022+Bobby-Gray@users.noreply.github.com>
2024-10-16 13:11:43 -0500

    Update impersonation_domain_scanner.py

--- impersonation_domain_scanner/impersonation_domain_scanner.py
+++ impersonation_domain_scanner/impersonation_domain_scanner.py
@@ -1,3 +1,4 @@
+from confusables import confusable_characters
 import dns.query
 import dns.resolver
 import dns.zone
@@ -14,12 +15,13 @@ import whois
 ## Impersonation Domain Scanner Script
 ## This script scans the past 14 days of newly registered domains (NRDs) for pattern matches based on two user inputs, must and optional.
 ### The 'must' input will be required for every permutation while the 'optional' values will be considered before or after the 'must' values as optional matches.
-### The script generates regex containing all potential 'confusable' characters then converts all NRD punycode where applicable. It also uses the original inputs to scan for levenshtein distance as domains are considered.
+### The script generates regex containing all potential 'confusable' characters then converts all NRD punycode where applicable. 
 ### Results are output to a dictionary then the script will attempt to resolve DNS a, nx, mx, registrar, and created_epoch records for the discovered domains.
 ### Final results are output to a json file titled, 'impersonation_domain_matches.json'.
 
-must_input = input("Enter comma + space separated list of strings that must be included in your domain search. E.g., for account.google.com, google must be included.\n") 
-opt_input = input("Enter comma + space separated list of optional strings to included in your domain search. E.g., for account.google.com, account or login or corporate are optional inclusions.\n (Default: ['account', 'activity', 'app', 'auth', 'cloud', 'corp', 'corporate', 'demo', 'duo', 'enterprise', 'gov', 'help', 'internal', 'login', 'mfa', 'okta', 'rsa', 'sign', 'signin', 'sso', 'support'])\n") or ['account', 'activity', 'app', 'auth', 'cloud', 'corp', 'corporate', 'demo', 'duo', 'enterprise', 'gov', 'help', 'internal', 'login', 'mfa', 'okta', 'rsa', 'sign', 'signin', 'sso', 'support', 'works']
+must_input = input("Enter comma + space separated list of strings that must be included in your domain search.\nE.g., for account.google.com, google must be included.\n") 
+opt_input = input("Enter comma + space separated list of optional strings to included in your domain search.\nE.g., for account.google.com, account or login or corporate are optional inclusions.\n (Default: ['account', 'activity', 'app', 'auth', 'cloud', 'corp', 'corporate', 'enterprise', 'help', 'internal', 'login', 'mfa', 'okta', 'sign', 'signin', 'sso', 'support'])\n") or ['account', 'activity', 'app', 'auth', 'cloud', 'corp', 'corporate', 'enterprise', 'help', 'internal', 'login', 'mfa', 'okta', 'sign', 'signin', 'sso', 'support']
+exclude_input = input("Enter comma + space separated list of exclusion strings to exclude from your levenshtein distance results.\nE.g., you want to see results impersonating 'nike' and receive frequent results for 'mike', exclude results matching 'mike'.\n") 
 
 if isinstance(must_input, str):
     must = must_input.split(', ')
@@ -29,12 +31,15 @@ if isinstance(opt_input, str):
     opt = opt_input.split(', ')
 if isinstance(opt_input, list):
     opt = opt_input
-print(f'must contain list: {must}')
-print(f'optional contain list: {opt}')
+if isinstance(exclude_input, str):
+    exclude = exclude_input.split(', ')
+if isinstance(exclude_input, list):
+    exclude = exclude_input
+print(f'must contain list: {must}\n')
+print(f'optional contain list: {opt}\n')
+print(f'levenshtein exclude list: {exclude}\n')
 ftd_url = 'https://raw.githubusercontent.com/xRuffKez/NRD/main/lists/14-day/domains-only/nrd-14day.txt'
-confusables_url = 'https://raw.githubusercontent.com/gc/confusables/master/src/characters.ts'
 ftd_filename = os.path.join(os.getcwd(), 'nrd_14day.txt')
-confusables_filename = os.path.join(os.getcwd(), 'characters.ts')
 
 def scrape_nrds(url, td_filename):
     print(f'Scraping domains registered in the past 14 days and checking for matches...')
@@ -49,7 +54,6 @@ def build_impersonation_regex(must, opt, td_filename):
     must_rex = ""
     pre_opt_rex = ""
     post_opt_rex = ""
-    confusables = {}
     imp_domains_matches = {}
     ld_list = []
     ### Build levenshtein distance pattern combinations
@@ -58,43 +62,19 @@ def build_impersonation_regex(must, opt, td_filename):
         for old in opt:
             ld_list.append(old + mld)
             ld_list.append(mld + old)
-    ### Pull confusables text file
-    r_confusables = requests.get(confusables_url, stream=True)
-    r_confusables.raise_for_status()  # Raise an exception for bad status codes
-    with open(confusables_filename, "wb") as file:
-        for chunk in r_confusables.iter_content(chunk_size=8192):
-            file.write(chunk)
-    with open(confusables_filename, "r") as file:
-        lines = file.readlines()
-        linecount = 0
-        linestart = 0
-        for i in lines:
-            if '([' in str(i):
-                linestart = linecount + 3
-            linecount += 1
-        pattern = r"'([^']*)', '([^']*)'"
-        for i in range(linestart, linecount-1):
-            match = re.search(pattern, lines[i])
-            if match:
-                val1 = str(match.group(1)) 
-                val2 = str(match.group(2))
-                confusables.update({val1: val2})
-            else:
-                continue
     ### Generate confusables character regex for 'must' list
     musts = []
     for md in must:
         ## Ex (([chars_1]){1}([chars_2]){1}([chars_3]){1})+
         must_rex_str = []
         for char in md:
-            for k,v in confusables.items():
-                if char == k:
-                    must_rex_val = ""
-                    must_rex_append = char
-                    for conf_char in v:
-                        must_rex_append = must_rex_append + conf_char
-                    must_rex_val = "([" + must_rex_append + "]){1}"
-                    must_rex_str.append(must_rex_val)
+            confus = confusable_characters(char)
+            must_rex_val = ""
+            must_rex_append = char
+            for conf_char in confus:
+                must_rex_append = must_rex_append + conf_char
+            must_rex_val = "([" + re.escape(must_rex_append) + "]){1}"
+            must_rex_str.append(must_rex_val)
         if len(must_rex_str) > 0:
             musts.append(str("(" + "".join(must_rex_str) + ")"))
     if len(musts) > 1:
@@ -108,14 +88,13 @@ def build_impersonation_regex(must, opt, td_filename):
         opt_rex_str = []
         for char in od:
         ### Ex (((([cC]){1}([oO]){1}([rR]){1}([pP]){1})|(([aA]){1}([cC]){1}([cC]){1}([oO]){1}([uU]){1}([nN]){1}([tT]){1}))+|\n)
-            for k,v in confusables.items():
-                if char == k:
-                    opt_rex_val = ""
-                    opt_rex_append = char
-                    for conf_char in v:
-                        opt_rex_append = opt_rex_append + conf_char
-                    opt_rex_val = "([" + opt_rex_append + "]){1}"
-                    opt_rex_str.append(opt_rex_val)
+            confus = confusable_characters(char)
+            opt_rex_val = ""
+            opt_rex_append = char
+            for conf_char in confus:
+                opt_rex_append = opt_rex_append + conf_char
+            opt_rex_val = "([" + re.escape(opt_rex_append) + "]){1}"
+            opt_rex_str.append(opt_rex_val)
         if len(opt_rex_str) > 0:
             pre_opts.append(str("(^" + "".join(opt_rex_str) + ")"))
             post_opts.append(str("(" + "".join(opt_rex_str) + "$)"))
@@ -141,11 +120,12 @@ def build_impersonation_regex(must, opt, td_filename):
             except Exception as e:
                 continue
             dn = str(i.strip())
+            excluded = any(x in str(nrd_dom) for x in exclude)
             for ld in ld_list:
                 if len(ld) > 4:
                     ld_match = distance(str(ld), str(nrd_dom))
-                    if ld_match < 2:
-                        print(dn)
+                    if ld_match < 2 and not excluded:
+                        print(f'levenshtein match: {dn}')
                         if ld not in imp_domains_matches.keys():
                             imp_domains_matches.update({ld : {'result' : []}})
                         ld_fuzzer = 'levenshtein_distance_' + str(ld_match)
@@ -159,11 +139,11 @@ def build_impersonation_regex(must, opt, td_filename):
                             'created_epoch': ''
                             }]
             match = RE_PATTERNS.search(nrd_dom)
-            if match:
+            if match and not excluded:
+                print(f'rex match: {dn}')
                 matched = match.group()
                 if matched not in imp_domains_matches.keys():
                     imp_domains_matches.update({matched : {'result' : []}})
-                print(dn)
                 re_in_dn_fuzzer = 'imp_domain_rex_match'
                 imp_domains_matches[matched]['result'] += [{
                     'fuzzer': str(re_in_dn_fuzzer),
@@ -228,6 +208,6 @@ def build_impersonation_regex(must, opt, td_filename):
     pprint(f'Impersonation domains discovered: {imp_domains_matches}')
     return imp_domains_matches
 
-
 if __name__=="__main__":
+    nrds = scrape_nrds(ftd_url, ftd_filename)
     imp_domains = build_impersonation_regex(must, opt, ftd_filename)
